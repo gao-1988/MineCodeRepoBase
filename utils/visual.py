71b5f138aac18ab3d05b3d08ed82083fdc63bb5c
板蓝根 <63853217+gao-1988@users.noreply.github.com>
2023-04-08 10:51:46 +0800

    Update visual.py

--- utils/visual.py
+++ utils/visual.py
@@ -1,10 +1,11 @@
 import random
-
+import matplotlib.pyplot as plt
 import open3d as o3d
 import numpy as np
 
 
 def show(*inputs, light_mode=False):
+    palette = [[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 1], [1, 1, 0], [1, 0, 1]]
     # 创建窗口对象
     vis = o3d.visualization.Visualizer()
     # 设置窗口标题
@@ -19,10 +20,12 @@ def show(*inputs, light_mode=False):
         opt.background_color = np.asarray([0, 0, 0])
     # 设置相机
     cam_control = vis.get_view_control()
+    color_index = 0
     for input in inputs:
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(input)
-        pcd.paint_uniform_color([random.uniform(0, 1), 0, random.uniform(0, 1)])
+        pcd.paint_uniform_color(palette[color_index])
+        color_index += 1
         vis.add_geometry(pcd)
     # cam_control.set_front(front=(1, 0, 0))
     vis.run()
@@ -135,11 +138,74 @@ def showRGBY(points1, points2, points3, points4, light_mode=False):
     vis.destroy_window()
 
 
+def show_matplot(points,
+                 title='Graph Title',
+                 xyz_labels=None):
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    x = points[:, 0]  # x position of point
+    y = points[:, 2]  # y position of point
+    z = points[:, 1]  # z position of point
+    ax.scatter(x, y, z, c=z)
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+    ax.set_zlim(-1, 1)
+    ax.set_title(title)
+    if xyz_labels is not None:
+        ax.set_xlabel(xyz_labels[0])
+        ax.set_ylabel(xyz_labels[1])
+        ax.set_zlabel(xyz_labels[2])
+    plt.show()
+
+def show_tow_matplot(*points,
+                 title='Graph Title',
+                 xyz_labels=None):
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    if len(points) == 1:
+        x = points[0][:, 0]  # x position of point
+        y = points[0][:, 2]  # y position of point
+        z = points[0][:, 1]  # z position of point
+        ax.scatter(x, y, z, c=z,marker='.')
+        ax.set_xlim(-1, 1)
+        ax.set_ylim(-1, 1)
+        ax.set_zlim(-1, 1)
+        ax.set_title(title)
+        if xyz_labels is not None:
+            ax.set_xlabel(xyz_labels[0])
+            ax.set_ylabel(xyz_labels[1])
+            ax.set_zlabel(xyz_labels[2])
+        plt.show()
+    else:
+        x_0 = points[0][:, 0]  # x position of point
+        y_0 = points[0][:, 2]  # y position of point
+        z_0 = points[0][:, 1]  # z position of point
+
+        x_1 = points[1][:, 0]  # x position of point
+        y_1 = points[1][:, 2]  # y position of point
+        z_1 = points[1][:, 1]  # z position of point
+        ax.scatter(x_0, y_0, z_0,c='r',marker='.')
+        ax.scatter(x_1, y_1, z_1, c='g',marker='.')
+        ax.set_xlim(-1, 1)
+        ax.set_ylim(-1, 1)
+        ax.set_zlim(-1, 1)
+        ax.set_title(title)
+        if xyz_labels is not None:
+            ax.set_xlabel(xyz_labels[0])
+            ax.set_ylabel(xyz_labels[1])
+            ax.set_zlabel(xyz_labels[2])
+        plt.show()
+
+
+
 if __name__ == '__main__':
     # data = np.genfromtxt('../data/1a04e3eab45ca15dd86060f189eb133.txt', delimiter=' ')[:, 0:3]
     # data2 = np.genfromtxt('../data/1a680e3308f2aac544b2fa2cac0778f5.txt', delimiter=' ')[:, 0:3]
     # show(data, data2)
 
-    showRGB(np.load('../result/best_model_predict/epoch_6/showed_p0.npy'),
-            np.load('../result/best_model_predict/epoch_6/showed_p1.npy'),
-            np.load('../result/best_model_predict/epoch_6/show_p_use_est_g.npy'), )
+    # showRGB(np.load('../result/best_model_predict/epoch_6/showed_p0.npy'),
+    #         np.load('../result/best_model_predict/epoch_6/showed_p1.npy'),
+    #         np.load('../result/best_model_predict/epoch_6/show_p_use_est_g.npy'), )
+
+    data = np.loadtxt('../test/data/airplane_0627.txt', delimiter=',')[:, 0:3]
+    show_matplot(data)
